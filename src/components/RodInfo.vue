93b2432af31a31d1636525068308085e0abb7769
Clovis <clovis@somar.co.nz>
2025-02-16 16:07:30 +1300

    only keep the name field for rods

--- src/components/RodInfo.vue
+++ src/components/RodInfo.vue
@@ -2,13 +2,7 @@
   <div class="rod">
     <div class="rod__name">
       <span class="rod__info-label"> Rod name: </span>
-      <span v-if="editRodName" class="rod__info-data">
-        {{ rod.name }}
-        <button @click="editRodName = !editRodName" class="icon-button">
-          <span class="material-symbols-outlined"> edit </span>
-        </button>
-      </span>
-      <span v-else class="rod__info-data">
+      <span class="rod__info-data">
         <!-- weird behaviour with the v-model, if you set the v-model to rod.name, it will lose the focus after each key press. I guess -->
         <input type="text" v-model="rodName" @change="rod.name = rodName" class="rod__name-input" />
       </span>
@@ -18,7 +12,7 @@
       <span class="rod__info-data"> {{ rod.autoPickTime }} seconds </span>
     </div>
     <div class="rod__ready-to-fish-time">
-      <span class="rod__info-label"> Re-baite after a catch: </span>
+      <span class="rod__info-label"> Re-bait after a catch: </span>
       <span class="rod__info-data"> {{ rod.readyToFishTime }} seconds </span>
     </div>
     <div class="rod__max-wait-time-fishing">
@@ -41,5 +35,5 @@ const props = defineProps({
 const { rod } = toRefs(props)
 
 const editRodName = ref(false)
-const rodName = ref('')
+const rodName = ref(rod.value.name)
 </script>
